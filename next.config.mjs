4f76ccb9b64ce19005fb2725abb450f7a3de0440
chaoshuai <q1319757657@163.com>
2026-07-24 11:33:30 +0800

    降低构建并发

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // 限制构建时的 worker 数量，从而从根源上降低 SSG 的页面并发生成度
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
